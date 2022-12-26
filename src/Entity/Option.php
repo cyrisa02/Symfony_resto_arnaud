37c5c7a85e8c5e30bd65b984c13a64958a202b90
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-12-26 15:58:12 +0100

    Add Menu-CRUD

--- src/Entity/Option.php
+++ src/Entity/Option.php
@@ -33,7 +33,10 @@ class Option
     {
         $this->menus = new ArrayCollection();
     }
-
+    public function __toString()
+     {
+         return $this->title; 
+    }
     public function getId(): ?int
     {
         return $this->id;
